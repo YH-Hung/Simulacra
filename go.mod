58494e621a9a72d96afb81c0bd538b8984704b21
yinghanhung <hle.infdy@gmail.com>
2026-07-11 00:02:06 +0800

    feat: status responses with typed google.rpc error details

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/bufbuild/protocompile v0.14.1
 	github.com/google/cel-go v0.29.2
 	github.com/spf13/cobra v1.10.2
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260414002931-afd174a4e478
 	google.golang.org/grpc v1.82.0
 	google.golang.org/protobuf v1.36.11
 	gopkg.in/yaml.v3 v3.0.1
@@ -23,5 +24,4 @@ require (
 	golang.org/x/sys v0.43.0 // indirect
 	golang.org/x/text v0.36.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20260414002931-afd174a4e478 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20260414002931-afd174a4e478 // indirect
 )
---
+++ internal/stub/status.go
@@ -0,0 +1,62 @@
+package stub
+
+import (
+	"encoding/json"
+	"fmt"
+
+	_ "google.golang.org/genproto/googleapis/rpc/errdetails"
+	spb "google.golang.org/genproto/googleapis/rpc/status"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/anypb"
+
+	"github.com/yinghanhung/simulacra/internal/schema"
+)
+
+type StatusSpec struct {
+	Code    string       `yaml:"code"`
+	Message string       `yaml:"message"`
+	Details []DetailSpec `yaml:"details"`
+}
+
+type DetailSpec struct {
+	Type  string         `yaml:"type"`
+	Value map[string]any `yaml:"value"`
+}
+
+func compileStatus(reg *schema.Registry, spec StatusSpec, allowOK bool) (*status.Status, error) {
+	if spec.Code == "" {
+		return nil, fmt.Errorf("status code is required")
+	}
+
+	encodedCode, err := json.Marshal(spec.Code)
+	if err != nil {
+		return nil, fmt.Errorf("encoding status code %q: %w", spec.Code, err)
+	}
+	var code codes.Code
+	if err := code.UnmarshalJSON(encodedCode); err != nil {
+		return nil, fmt.Errorf("invalid status code %q: %w", spec.Code, err)
+	}
+	if code == codes.OK && !allowOK {
+		return nil, fmt.Errorf("status code OK is not allowed here")
+	}
+
+	protoStatus := &spb.Status{Code: int32(code), Message: spec.Message}
+	for i, detail := range spec.Details {
+		desc, err := reg.LookupMessage(detail.Type)
+		if err != nil {
+			return nil, fmt.Errorf("details[%d] type %q: %w", i, detail.Type, err)
+		}
+		msg, err := BuildMessage(reg.Types(), desc, detail.Value)
+		if err != nil {
+			return nil, fmt.Errorf("details[%d] type %q: %w", i, detail.Type, err)
+		}
+		packed, err := anypb.New(msg)
+		if err != nil {
+			return nil, fmt.Errorf("details[%d] type %q: packing detail: %w", i, detail.Type, err)
+		}
+		protoStatus.Details = append(protoStatus.Details, packed)
+	}
+
+	return status.FromProto(protoStatus), nil
+}
